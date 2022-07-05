6dc3532878dd35bc5e98d4fa9baaec39d61f391d
Kipkorir74 <49982743+Kipkorir74@users.noreply.github.com>
2022-07-05 13:33:53 +0300

    further refinement

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Weather from './weather.js'
 function App() {
   return (
     <div className="app">
+      
      <Weather/>
     </div>
   );
--- src/weather.js
+++ src/weather.js
@@ -2,33 +2,77 @@ import React,{useState} from "react";
 import axios from "axios";
 
 const Weather = () => {
-    
-    const url='https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=8b037a43dff0caa8cf3d47de56123337';
+    const [data,setData]=useState({});
+    const [location, setLocation]=useState('')
+
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=8b037a43dff0caa8cf3d47de56123337`;
+
+    //define a search input function to handle search icon
+    const searchLocation = (e) =>{
+
+        // eslint-disable-next-line no-restricted-globals
+        if(e.key ==="Enter"){
+            axios.get(url)
+            .then((response)=>{
+                setData(response.data);
+                console.log(response.data);
+            })
+            setLocation('')
+        }
+        
+      
+    }
     return ( 
+        <div className="hk">
+           <div className="search">
+            <input type="text" 
+                value={location} 
+                onChange={e => setLocation(e.target.value)} 
+                placeholder='Enter Your Location'
+                onKeyPress={searchLocation}
+                />
+            </div> 
+        
         <div className="container">
+
             <div className="top">
                 <div className="location">
-                    <p>Nairobi</p>
+                    <p>{data.name}</p>
                 </div>
                 <div className="temp">
-                    <h1>31°C</h1>
+                     {data.main ? <h1>{data.main.temp} °C</h1>:null}
+                
                 </div>
                 <div className="description">
-                    Cloudy
+                    {data.weather ? <p>{data.weather[0].main}</p> :null}
+                    
                 </div>
             </div>
+            {data.name !=undefined && 
             <div className="bottom">
                 <div className="feels">
-                    <p>35°C</p>
+                    
+                        {data.main ? <p>{data.main.feels_like} °C</p> : null}
+                        
+                    <p>Temperature Inafeel Kama</p>
                 </div>
+
                 <div className="humidity">
-                    <p>58%</p>
+                    {data.main ?<p className="bold">{data.main.humidity}%</p>:null}
+                    <p>Humidity</p>
                 </div>
+
                 <div className="wind">
-                    <p>13 km/h</p>
+                    {data.wind ?<p className="bold">{data.wind.speed} ms</p>:null}
+                    <p>Wind</p>
                 </div>
+
             </div>
+            }
+            
         </div>
+        </div>
+   
      );
 }
  
